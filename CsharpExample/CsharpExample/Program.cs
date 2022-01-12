82a55964233d1257a0615fdcc9f304f9409d09da
Nicole Ajoy <najoy@lockstep.io>
2022-01-12 17:41:22 -0500

    Working demo program, fetching invoices

--- CsharpExample/CsharpExample/Program.cs
+++ CsharpExample/CsharpExample/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 using LockstepSDK;
-using RestSharp.Serialization.Json;
 
 namespace LockstepExamples // Note: actual namespace depends on the project name.
 {
@@ -10,17 +11,36 @@ namespace LockstepExamples // Note: actual namespace depends on the project name
     {
         public static async Task Main(string[] args)
         {
-            var serializer = new JsonSerializer();
             var client = LockstepApi
-                .withEnvironment(LockstepEnv.SBX);
-            var apiKey = Environment.GetEnvironmentVariable("LOCKSTEPAPI");
+                .WithEnvironment(LockstepEnv.SBX);
+            var apiKey = Environment.GetEnvironmentVariable("LOCKSTEPAPI_SBX");
             if (apiKey != null)
             {
-                client.withApiKey(apiKey);
+                client.WithApiKey(apiKey);
             }
             var result = await client.Status.Ping();
             
-            Console.WriteLine("Ping result: " + serializer.Serialize(result));
+            Console.WriteLine("Ping result: " + JsonSerializer.Serialize(result));
+            
+            var pageNumber = 0;
+            var count = 0;
+            
+            while (true)
+            {
+                var invoices = await client.Invoices.QueryInvoices("invoiceDate > 2021-12-01", null, "invoiceDate asc", 100, pageNumber);
+                if (!invoices.Success || invoices.Value.Records.Length == 0)
+                {
+                    break;
+                }
+                foreach (var invoice in invoices.Value.Records)
+                {
+                    Console.WriteLine($"Invoice {count++}: {invoice.InvoiceId}");
+                }
+
+                pageNumber++;
+            }
+            
+            
         }
     }
 }
