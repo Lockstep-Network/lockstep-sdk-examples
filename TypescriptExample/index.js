a7fbacd9896a2663a2fb2ea565a0bf47ebe26366
Ted Spence <tspence@lockstep.io>
2022-01-13 11:00:31 -0800

    Working on JavaScript / TypeScript example project

---
+++ TypescriptExample/index.js
@@ -0,0 +1,10 @@
+function main()
+{
+    console.log('Hello, world');   
+    // var client = LockstepApi.withEnvironment('prd');
+    // client.Status.ping().then(result => {
+    //     console.log('Result: ' + JSON.stringify(result));
+    // })
+}
+
+main();
--- TypescriptExample/main.ts
+++
@@ -1,9 +0,0 @@
-import { LockstepApi } from 'lockstep-sdk';
-
-export function main()
-{
-    var client = LockstepApi.withEnvironment('prd');
-    client.Status.ping().then(result => {
-        console.log('Result: ' + JSON.stringify(result));
-    })
-}
