8a598a047ebf80321e91c83d470c3ee91b305933
jsandov <46768834+jsandov@users.noreply.github.com>
2022-01-19 16:02:50 -0800

    adding the suggested changes per Ted (#16)
    Co-authored-by: Jesus Sandoval <jsandoval@lockstep.io>

--- PythonExample/main.py
+++ PythonExample/main.py
@@ -12,8 +12,7 @@ def retrieve_api_key():
 
 
 def create_client(apikey):
-    #     create client
-    env = 'https://api.sbx.lockstep.io/'
+    env = 'sbx'
     client = LockstepApi(env)
     client.with_api_key(apikey)
     if not client:
@@ -27,36 +26,32 @@ def main():
     API_KEY = retrieve_api_key()
     client = create_client(API_KEY)
 
-    # Ping
     status_results = client.status.ping()
     print(f"StatusResult: {status_results}")
 
     page_num = 0
     count = 1
 
-    while page_num < 5: # replace with True, when SDK is working, currently, it is in an endless loop otherwise
-        # Single API call to fetch invoices and company info.
-        # 1/18/2022 Currently SDK query parameters don't work like expected.
-        # Customer Name field is also not available.
+    while page_num < 5:
         invoices = client.invoices.query_invoices(
             "invoiceDate GT 2021-01-10 AND invoiceDate LT 2021-03-10",
             "Company",
             "invoiceDate asc",
             100,
-            20)
+            page_num)
 
         if len(invoices['records']) == 0:
             break
 
         for record in invoices['records']:
             print(f"Invoice {count}: {record['invoiceId']}")
-            print(f"Company name: {record['companyId']}") # record['company']['companyName']
+            print(f"Company name: {record['companyId']}")
             print(f"Outstanding Balance: ${record['outstandingBalanceAmount']} \n")
             count += 1
 
         page_num += 1
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
