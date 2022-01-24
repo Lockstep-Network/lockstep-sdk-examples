8370461b585bcf780fafd5fe008297d883bfecb5
venkattejagaddam <68100466+venkattejagaddam@users.noreply.github.com>
2022-01-24 10:48:47 -0700

    Added Company Report Example code.

---
+++ TypescriptExample/companyReport.ts
@@ -0,0 +1,42 @@
+import { LockstepApi } from './node_modules/lockstep-sdk/src/LockstepApi';
+
+console.log("Creating client");
+//var clients=process.withEnvironment. 
+var client = LockstepApi.withEnvironment("sbx").withApiKey("LSPK-plefvN72pvVPR+4r47MKyfef4U4hV5WzB4YMDRWnOOjuOSHsgX7Hvi1Q6rIp4lD+obF+HS+JsycqrjsZRF5Kqg==");
+console.log("About to call ping");
+
+console.log("Started ping call");
+
+//function to fetch companyNames
+async function company() 
+{
+    try 
+    {
+        var companies = await client.Companies.queryCompanies("companyName startswith 'a' OR companyName startswith 'b'","Invoices","ASC",100,0);
+    }
+    catch (error) 
+    {
+        console.error(error);
+    }    
+    var pageNumbers = 0;
+    while (pageNumbers<10) 
+    {
+        var companies = await client.Companies.queryCompanies("companyName startswith 'a' OR companyName startswith 'b'","Invoices","ASC",100,pageNumbers);
+        if(!companies.success || companies.value.records.length == 0) 
+        {
+            break;
+        }
+
+        companies.value.records.forEach(company => 
+        {
+            console.log("company Name:", company.companyName);
+            console.log("company PhoneNumber:", company.phoneNumber);
+            console.log("company apEmailAddress:", company.apEmailAddress);
+            console.log("company arEmailAddress:", company.arEmailAddress);
+            console.log(" ");
+        });
+
+        pageNumbers++;
+    }
+}
+company();
