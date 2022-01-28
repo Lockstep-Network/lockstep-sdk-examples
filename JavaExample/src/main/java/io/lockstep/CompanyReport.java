9c3db967f710e21fa5131283200ae7511d8d02de
michaelmaxyz <70110919+michaelmaxyz@users.noreply.github.com>
2022-01-27 20:23:02 -0500

    Add files via upload

--- JavaExample/src/main/java/io/lockstep/CompanyReport.java
+++ JavaExample/src/main/java/io/lockstep/CompanyReport.java
@@ -12,62 +12,73 @@ import io.lockstep.api.models.LockstepResponse;
 import io.lockstep.api.models.StatusModel;
 
 public class CompanyReport {
-   public static void main(String[] args) throws IOException {
 
-    Map<String, String> environment = System.getenv();
-    String key = environment.getOrDefault("LOCKSTEPAPI_SBX", null);
-    LockstepApi client = LockstepApi.withEnvironment("sbx");
-    
-    if (key != null) {
-        client.withApiKey(key);
+    private String fileName;
+
+    public CompanyReport(String fileName) {
+        this.fileName = fileName;
     }
+
+    public void writeToFile() throws IOException{
+
+        Map<String, String> environment = System.getenv();
+        String key = environment.getOrDefault("LOCKSTEPAPI_SBX", null);
+        LockstepApi client = LockstepApi.withEnvironment("sbx");
+        
+        //if ApiKey exists, use it
+        if (key != null) {
+            client.withApiKey(key);
+        }
+        
+        LockstepResponse<StatusModel> response = client.getStatusClient().ping();
+        
+        //check if LockstepAPI is connected correctly
+        System.out.println("Ping result: " + response.isSuccess());
+        CompaniesClient companiesClient = client.getCompaniesClient();
     
-    LockstepResponse<StatusModel> response = client.getStatusClient().ping();
-    
-    //check if LockstepAPI is connected
-    System.out.println("Ping result: " + response.isSuccess());
-    CompaniesClient companiesClient = client.getCompaniesClient();
+        int pageNum = 0;
 
-    int pageNum = 0;
+        List<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
+        LockstepResponse<FetchResult<CompanyModel>> companies;
+        CompanyModel[] companyModelList;
 
-    List<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
-    LockstepResponse<FetchResult<CompanyModel>> companies;
-    CompanyModel[] companyModelList;
+        do{
+            companies = companiesClient.queryCompanies("", "", "CompanyName asc", 100, pageNum);
+            FetchResult<CompanyModel> fetch = companies.getValue();
+            companyModelList = fetch.getRecords();
 
-    do{
-        companies = companiesClient.queryCompanies("", "", "CompanyName asc", 100, pageNum);
-        FetchResult<CompanyModel> fetch = companies.getValue();
-        companyModelList = fetch.getRecords();
+            //write out data for each row
+            for (CompanyModel currentModel : companyModelList) {
+                ArrayList<String> row = new ArrayList<String>();
+                row.add(currentModel.getCompanyName());
+                row.add(currentModel.getPhoneNumber());
+                row.add(currentModel.getApEmailAddress());
+                rows.add(row);
+            }
+            pageNum++;
+        //repeat until we are out of companies
+        } while (companies.isSuccess() && companyModelList.length > 0);
 
-        //write out data for each row
-        for (CompanyModel currentModel : companyModelList) {
-            ArrayList<String> row = new ArrayList<String>();
-            row.add(currentModel.getCompanyName());
-            row.add(currentModel.getPhoneNumber());
-            row.add(currentModel.getApEmailAddress());
-            rows.add(row);
+        //write out the first row
+        FileWriter csvWriter = new FileWriter(fileName);
+        csvWriter.append("Name");
+        csvWriter.append(",");
+        csvWriter.append("Phone Number");
+        csvWriter.append(",");
+        csvWriter.append("Email Address");
+        csvWriter.append("\n");
+
+        //write out every company for each subsequent row
+        for (ArrayList<String> currentRow : rows) {
+            csvWriter.append(String.join(",", currentRow));
+            csvWriter.append("\n");
         }
 
-        pageNum++;
-    //repeat until we are out of companies
-    } while (companies.isSuccess() && companyModelList.length > 0);
-
-    //write out the first row
-    FileWriter csvWriter = new FileWriter("companies.csv");
-    csvWriter.append("Name");
-    csvWriter.append(",");
-    csvWriter.append("Phone Number");
-    csvWriter.append(",");
-    csvWriter.append("Email Address");
-    csvWriter.append("\n");
-
-    //write out every company for each row
-    for (ArrayList<String> currentRow : rows) {
-        csvWriter.append(String.join(",", currentRow));
-        csvWriter.append("\n");
+        csvWriter.flush();
+        csvWriter.close();
     }
 
-    csvWriter.flush();
-    csvWriter.close();
-   } 
+    public static void main(String[] args) throws IOException {
+        new CompanyReport("companies.csv").writeToFile();
+    }
 }
