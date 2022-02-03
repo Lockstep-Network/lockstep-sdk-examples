78ca2974f4683c651bde9543dbe7c93f56e83450
Ted Spence <ted@spence.net>
2022-02-03 13:50:43 -0800

    Clean up TypeScript example code (#31)
    * Removed unused sdk_files folder, updated package lock
    
    * Updated examples to match latest SDK builds
    
    * Clean up typescript examples for readability
    
    Co-authored-by: Ted Spence <tspence@lockstep.io>

--- TypescriptExample/companyReport.ts
+++ TypescriptExample/companyReport.ts
@@ -1,50 +1,56 @@
-import { LockstepApi } from 'lockstep-sdk';
+import * as lockstep from 'lockstep-sdk';
 
-console.log("Creating client");
-//Api-key
-var client = LockstepApi.withEnvironment("sbx").withApiKey("Api-key");
-console.log("About to call ping");
+// Note that csv-writer requires ES5 mode
+import * as csv from 'csv-writer';
 
-console.log("Started ping call");
-//writing to csv file
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-    path: './companyData.csv',
-    header: [
-        {id: 'companyName', title: 'Company Name'},
-        {id: 'phone', title: 'Phone Number'},
-        {id: 'apEmail', title: 'apEmailAddress'},
-        {id: 'arEmail', title: 'arEmailAddress'}
-    ]
-});
-var records=[];
+/**
+ * 2022-02-03 Lockstep
+ * 
+ * Example program to generate a report for invoices for your Lockstep Platform account.
+ * - Prints output to the console AND writes to a CSV file.
+ * - Prints the first ten pages of data.
+ * - Uses an environment variable to keep your API key out of the source code.
+ * 
+ * To run this program, compile it with `tsc --target ES5 companyReport.ts` then run it with `node companyReport.js`.
+ * 
+ * @param apiKey 
+ * @param queryFilter 
+ */async function generateCompanyReport(apiKey: string, queryFilter: string) {
 
-//function to fetch companyNames
-async function company() 
-{
-    try 
-    {
-        var companies = await client.Companies.queryCompanies("companyName startswith 'a' OR companyName startswith 'b'","Invoices","ASC",100,0);
-    }
-    catch (error) 
-    {
-        console.error(error);
-    }    
-    var pageNumbers = 0;
-    while (pageNumbers<10) 
-    {
-        var companies = await client.Companies.queryCompanies("companyName startswith 'a' OR companyName startswith 'b'","Invoices","ASC",100,pageNumbers);
-        if(!companies.success || companies.value.records.length == 0) 
-        {
+    // Create a client
+    var client = lockstep.LockstepApi
+        .withEnvironment("sbx")
+        .withApiKey(apiKey);
+
+    // Create a CSV file for the results
+    const csvWriter = csv.createObjectCsvWriter({
+        path: './companyData.csv',
+        header: [
+            { id: 'companyName', title: 'Company Name' },
+            { id: 'phone', title: 'Phone Number' },
+            { id: 'apEmail', title: 'apEmailAddress' },
+            { id: 'arEmail', title: 'arEmailAddress' }
+        ]
+    });
+
+    // This will be our file format
+    var records: {
+        companyName: string; phone: string | null; apEmail: string | null; arEmail: string | null;
+        }[] = [];
+
+    // Query the first ten pages of results for this filter
+    var pageNumber = 0;
+    while (pageNumber < 10) {
+        var companies = await client.Companies.queryCompanies(queryFilter, undefined, undefined, 100, pageNumber);
+        if (!companies.success || !companies?.value?.records) {
             break;
         }
 
-        companies.value.records.forEach(async company => 
-        {
-            var companyName=company.companyName;
-            var phone=company.phoneNumber;
-            var apEmail=company.apEmailAddress;
-            var arEmail=company.arEmailAddress;
+        companies.value.records.forEach(async company => {
+            var companyName = company.companyName;
+            var phone = company.phoneNumber;
+            var apEmail = company.apEmailAddress;
+            var arEmail = company.arEmailAddress;
 
             console.log("company Name:", companyName);
             console.log("company PhoneNumber:", phone);
@@ -52,11 +58,14 @@ async function company()
             console.log("company arEmailAddress:", arEmail);
             console.log(" ");
 
-            records.push({companyName: companyName,  phone:phone, apEmail:apEmail, arEmail:arEmail});
+            records.push({ companyName: companyName, phone: phone, apEmail: apEmail, arEmail: arEmail });
             await csvWriter.writeRecords(records);
         });
 
-        pageNumbers++;
+        pageNumber++;
     }
 }
-company();
+
+// Fetch the API key from an environment variable
+var apiKey = process.env["LOCKSTEPAPI_SBX"] || "Fill in your API key here";
+generateCompanyReport(apiKey, "companyName startswith a OR companyName startswith B");
--- TypescriptExample/index.ts
+++ TypescriptExample/index.ts
@@ -1,41 +1,44 @@
+import * as lockstep from 'lockstep-sdk';
 
-import { LockstepApi } from 'lockstep-sdk';
+/**
+ * 2022-02-03 Lockstep
+ * 
+ * Example program to generate a report for invoices for your Lockstep Platform account.
+ * - Prints output to the console.
+ * - Prints the first ten pages of data.
+ * - Uses an environment variable to keep your API key out of the source code.
+ * 
+ * To run this program, compile it with `tsc index.ts` then run it with `node index.js`.
+ * 
+ * @param apiKey 
+ * @param queryFilter 
+ */
+async function generateInvoiceReport(apiKey: string, queryFilter: string) {
 
-console.log("Creating client");
-//Api-key
+    // Create a client
+    var client = lockstep.LockstepApi
+        .withEnvironment("sbx")
+        .withApiKey(apiKey);
 
-var client = LockstepApi.withEnvironment("sbx").withApiKey("Api-key");
-console.log("About to call ping");
-
-console.log("Started ping call");
-async function invoice() {
-    try 
-    {
-        var invoices = await client.Invoices.queryInvoices("invoiceDate > 2021-12-01", "Customer", "invoiceDate asc", 200, 0);
-    }
-    catch (error) 
-    {
-        console.error(error);
-    }    
-    var pageNumbers = 0;
-    while (pageNumbers<10) 
-    {
-        var invoices = await client.Invoices.queryInvoices("invoiceDate > 2021-12-01", "Customer", "invoiceDate asc", 200, pageNumbers);
-        if(!invoices.success || invoices?.value?.records == null) 
-        {
+    // Fetch the first ten pages of data for this query filter
+    var pageNumber = 0;
+    while (pageNumber < 10) {
+        var invoices = await client.Invoices.queryInvoices(queryFilter, "Customer", "invoiceDate asc", 200, pageNumber);
+        if (!invoices.success || !invoices?.value?.records) {
             break;
         }
 
-        invoices.value.records.forEach(invoice => 
-        {
+        invoices.value.records.forEach(invoice => {
             console.log("Invoice Id:", invoice.invoiceId);
             console.log("OutStanding Amount:", invoice.outstandingBalanceAmount);
             console.log("Customer Company Name:", invoice?.company?.companyName);
             console.log(" ");
         });
 
-        pageNumbers++;
+        pageNumber++;
     }
 }
 
-invoice();
+// Fetch the API key from an environment variable
+var apiKey = process.env["LOCKSTEPAPI_SBX"] || "Fill in your API key here";
+generateInvoiceReport(apiKey, "invoiceDate > 2021-12-01");
