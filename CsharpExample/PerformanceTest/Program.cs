827302ed4feb82f71bd8b711c47bca91b5a3018a
Ted Spence <tspence@lockstep.io>
2022-03-11 09:44:34 -0800

    Improvements to C# SDK programs

---
+++ CsharpExample/PerformanceTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using LockstepSDK;
+
+namespace LockstepExamples 
+{
+    public static class PerformanceTest
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = LockstepApi.WithEnvironment(LockstepEnv.SBX)
+                .WithApiKey(Environment.GetEnvironmentVariable("LOCKSTEPAPI_SBX"));
+
+            // Test first API call
+            var result = await client.Status.Ping();
+            if (!result.Success || !result.Value.LoggedIn)
+            {
+                Console.WriteLine("Your API key is not valid.");
+                Console.WriteLine("Please set the environment variable LOCKSTEPAPI_SBX and try again.");
+                return;
+            }
+
+            // Basic diagnostics
+            Console.WriteLine($"Ping result: {result.Value.UserName} ({result.Value.UserStatus})");
+            Console.WriteLine($"Server status: {result.Value.Environment} {result.Value.Version}");
+            Console.WriteLine();
+            
+            // Run a series of a thousand queries for invoices
+            var performance = new List<Tuple<long, int>>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var pageNumber = 0;
+
+                while (true)
+                {
+                    var invoices = await client.Invoices.QueryInvoices(
+                        null,
+                        null,
+                        null,
+                        100,
+                        pageNumber
+                    );
+                    performance.Add( new Tuple<long, int>( invoices.TotalRoundtrip, invoices.ServerDuration));
+
+                    if (!invoices.Success || invoices.Value?.Records?.Length == 0) break;
+                    pageNumber++;
+                }
+            }
+            
+            // Sum up the performance reports
+            var averageRoundtrip = (from t in performance select t.Item1).Average();
+            var averageServerDuration = (from t in performance select t.Item2).Average();
+            Console.WriteLine($"Number of calls: {performance.Count}");
+            Console.WriteLine($"Average roundtrip: {averageRoundtrip}");
+            Console.WriteLine($"Average server: {averageServerDuration}");
+            Console.WriteLine("Done");
+        }
+    }
+}
