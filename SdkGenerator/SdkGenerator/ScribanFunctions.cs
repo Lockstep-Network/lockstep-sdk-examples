3a496920547530cf5a74114883d59617a41603fc
Alex Russak <arussak@lockstep.io>
2022-05-19 14:33:06 -0700

    Fix up namespaces & net6 upgrade

--- SdkGenerator/SdkGenerator/ScribanFunctions.cs
+++ SdkGenerator/SdkGenerator/ScribanFunctions.cs
@@ -4,32 +4,32 @@ using System.Threading.Tasks;
 using Scriban;
 using Scriban.Runtime;
 using Scriban.Syntax;
-using SwaggerDownload.Project;
+using SdkGenerator.Project;
+using SdkGenerator.Schema;
 
-namespace SwaggerDownload
+namespace SdkGenerator;
+
+public static class ScribanFunctions
 {
-    public static class ScribanFunctions
+    public static async Task ExecuteTemplate(string templateName, ProjectSchema project, ApiSchema api, string outputFile)
     {
-        public static async Task ExecuteTemplate(string templateName, ProjectSchema project, ApiSchema api, string outputFile)
+        try
         {
-            try
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
-                var templateText = await File.ReadAllTextAsync(templateName);
-                var template = Template.Parse(templateText);
-                var scriptObject1 = new ScriptObject();
-                scriptObject1.Import(typeof(Extensions));
-                var context = new TemplateContext();
-                context.PushGlobal(scriptObject1);
-                context.SetValue(new ScriptVariableGlobal("api"), api);
-                context.SetValue(new ScriptVariableGlobal("project"), project);
-                var result = await template.RenderAsync(context);
-                await File.WriteAllTextAsync(outputFile, result);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+            var templateText = await File.ReadAllTextAsync(templateName);
+            var template = Template.Parse(templateText);
+            var scriptObject1 = new ScriptObject();
+            scriptObject1.Import(typeof(Extensions));
+            var context = new TemplateContext();
+            context.PushGlobal(scriptObject1);
+            context.SetValue(new ScriptVariableGlobal("api"), api);
+            context.SetValue(new ScriptVariableGlobal("project"), project);
+            var result = await template.RenderAsync(context);
+            await File.WriteAllTextAsync(outputFile, result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
         }
     }
 }
